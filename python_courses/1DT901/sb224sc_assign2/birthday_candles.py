6d2758221e6e05d1f935173a3ef030ec57075cb8
s0qz <samuelberg2001@gmail.com>
2022-09-04 19:11:49 +0200

    Added some .py files for assign2 with problem

---
+++ python_courses/1DT901/sb224sc_assign2/birthday_candles.py
@@ -0,0 +1,25 @@
+'''Problem:
+Birthday  (VG)
+Write a program birthday_candles.py that computes how many boxes of candles a person needs to buy each year for her\his birthday cake.
+You can assume that the person reaches an age of 100, the number of candles used each year is the same as the age,
+that you save non-used candles from one year to another, and that each box contains 24 candles.
+Also, at the end, we want you to print the total number of boxes one has to buy, and how many candles that are available after having celebrated the 100th birthday.
+'''
+'''Desired output examples:
+Before birthday 1, buy 1 box(es)
+Before birthday 7, buy 1 box(es)
+Before birthday 10, buy 1 box(es)
+Before birthday 12, buy 1 box(es)
+Before birthday 14, buy 1 box(es)
+
+...
+
+Before birthday 95, buy 3 box(es)
+Before birthday 96, buy 4 box(es)
+Before birthday 97, buy 5 box(es)
+Before birthday 98, buy 4 box(es)
+Before birthday 99, buy 4 box(es)
+Before birthday 100, buy 4 box(es)
+
+Total number of boxes: 211, Remaining candles: 14
+'''
---
+++ python_courses/1DT901/sb224sc_assign2/random_numbers.py
@@ -0,0 +1,13 @@
+'''Problem:
+Write a program random_numbers.py that reads a positive integer n from the keyboard and then:
+Generates and prints (in a single line) n random numbers in the interval [1,100]
+Prints the average value (with two decimals), the smallest number (min), and the largest number (max).
+A suitable error message should be presented if the input number n is non-positive.
+You should not use a list (or any other data structure) to first store the generated numbers before you compute average, min, and max.
+'''
+'''Desired output examples:
+Enter number of integers to be generated: 10
+
+Generated values: 77 15 13 54 96 73 100 12 98 28 
+Average, min, and max are 56.6, 12, and 100
+'''
