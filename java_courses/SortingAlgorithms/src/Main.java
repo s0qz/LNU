71fa0d7b94ff078b43d58d53ab3997b841ae1d57
s0qz <samuelberg2001@gmail.com>
2022-09-04 18:44:02 +0200

    Added comments to the main class

--- java_courses/SortingAlgorithms/src/Main.java
+++ java_courses/SortingAlgorithms/src/Main.java
@@ -1,16 +1,15 @@
-import java.lang.Math;
-import java.util.concurrent.TimeUnit;
-
-import org.apache.commons.lang3.time.StopWatch;
+import java.lang.Math;                              // Importing the math library
+import java.util.concurrent.TimeUnit;               // Importing the timeunit library
+import org.apache.commons.lang3.time.StopWatch;     // Importing the stopwatch library
 
 public class Main {
     public static void main(String[] args) {
-        double[] bubbleSort = new double[100000];
+        double[] bubbleSort = new double[100000];   // Creating four arrays to store data points in
         double[] insertionSort = new double[100000];
         double[] mergeSort = new double[100000];
         double[] quickSort = new double[100000];
 
-        for (int i = 0; i < 100000; i++)
+        for (int i = 0; i < 100000; i++)            // These four for loops fill all the arrays with random data points
             bubbleSort[i] = Math.random();
 
         for (int i = 0; i < 100000; i++)
@@ -22,15 +21,16 @@ public class Main {
         for (int i = 0; i < 100000; i++)
             quickSort[i] = Math.random();
 
-        StopWatch stopWatch = new StopWatch();
+        StopWatch stopWatch = new StopWatch();      // Generates a stopwatch
 
+        // Creates time variables one for each type of sorting algorithm
         double bubbleTime, insertionTime, mergeTime, quickTime;
 
-        stopWatch.start();
-        Bubblesort.bubbleSort(bubbleSort);
-        stopWatch.stop();
-        bubbleTime = stopWatch.getTime(TimeUnit.MILLISECONDS);
-        stopWatch.reset();
+        stopWatch.start();                          // Starts the stopwatch
+        Bubblesort.bubbleSort(bubbleSort);          // Calls the class.function
+        stopWatch.stop();                           // Stops the stopwatch
+        bubbleTime = stopWatch.getTime(TimeUnit.MILLISECONDS);  // Gets the elapsed time in the milliseconds unit
+        stopWatch.reset();                          // Resets the stopwatch
         System.out.println("Bubble sort Done!");
 
         stopWatch.start();
@@ -43,7 +43,7 @@ public class Main {
         stopWatch.start();
         Mergesort.mergeSort(mergeSort);
         stopWatch.stop();
-        mergeTime = stopWatch.getTime(TimeUnit.MICROSECONDS);
+        mergeTime = stopWatch.getTime(TimeUnit.MICROSECONDS);   // Gets the elapsed time in the microseconds unit
         stopWatch.reset();
         System.out.println("Mergesort Done!");
 
@@ -54,6 +54,7 @@ public class Main {
         stopWatch.reset();
         System.out.println("Quicksort Done!");
 
+        // Prints the result times for each sorting algorithm
         System.out.println("Bubble sort: " + bubbleTime + " milliseconds");
         System.out.println("Insertion sort: " + insertionTime + " milliseconds");
         System.out.println("Mergesort: " + mergeTime + " microseconds");
