1b66fd984d7674b0d8e400f6af5ed32379f470f1
s0qz <samuelberg2001@gmail.com>
2022-09-08 08:01:16 +0200

    fixed random_numbers.py

--- python_courses/1DT901/sb224sc_assign2/random_numbers.py
+++ python_courses/1DT901/sb224sc_assign2/random_numbers.py
@@ -2,19 +2,26 @@
 #
 # Author: Samuel Berg
 # Date: 05-Sep-2022
-import random
+from random import randint
 
 
 def random_numbers(N):
     for_average = 0
+    current_value = 0
+    minimum = 101
+    maximum = 0
     print('Generated values: ', end='')
-    lst = [random.randint(1, 100) for i in range(N)]
-    for value in lst:
-        for_average += value
-        print(value, end=' ')
+    for _ in range(N):
+        current_value = randint(1, 100)
+        for_average += current_value
+        print(current_value, end=' ')
+        if current_value < minimum:
+            minimum = current_value
+        elif current_value > maximum:
+            maximum = current_value
     avg = round(for_average / N)
     print(
-        f'\nAverage, min, and max are {avg}, {min(lst)}, and {max(lst)}')
+        f'\nAverage, min, and max are {avg}, {minimum}, and {maximum}')
 
 
 N = -1
