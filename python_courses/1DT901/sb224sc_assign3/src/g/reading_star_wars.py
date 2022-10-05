07707d8406d3f5ab873fec161237babde03e887b
s0qz <samuelberg2001@gmail.com>
2022-10-05 10:57:40 +0200

    made it so reading_star_wars.py prints exactly like shown in exercise

--- python_courses/1DT901/sb224sc_assign3/src/g/reading_star_wars.py
+++ python_courses/1DT901/sb224sc_assign3/src/g/reading_star_wars.py
@@ -13,6 +13,7 @@ with open(path + file, 'r') as f:
         line = line.strip('\n')
         line = line.split(', ')
         person = Character.Character(line[0], line[1], line[2])
-        print(person.get_name(), '\t\t', person.get_kind(),
-              ' ' * (16 - len(person.get_kind())), person.get_planet())
+        print(person.get_name(), ' ' * (22 - len(person.get_name())),
+              person.get_kind(), ' ' * (14 - len(person.get_kind())),
+              person.get_planet())
         counter += 1
