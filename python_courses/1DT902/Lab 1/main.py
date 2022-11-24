193bcaaae3c316e818d89a0d70fe7a8ed84851ba
Samuel Berg <sb224sc@student.lnu.se>
2022-11-24 16:25:54 +0100

    ...

--- python_courses/1DT902/Lab 1/main.py
+++ python_courses/1DT902/Lab 1/main.py
@@ -1,9 +1,8 @@
 from machine import Pin
 from picozero import Speaker, TemperatureSensor
-from time import sleep, time
+from time import ticks_ms
 from math import log, pow
 import dht
-import _thread
 
 
 def temp_conversion(voltage):
@@ -17,68 +16,66 @@ def temp_conversion(voltage):
     return (1 / (A + B_log + C_pow)) - 273.15
 
 
-def mario_song():
-    global mario
+def temperature():
+    print(f"Temprature: {round(analog_temp.temp)} °C")
+    d.measure()
+    print(f"Temprature: {d.temperature()} °C and Humidity: {d.humidity()} %")
+
+
+count = 0
+time_difference = 0
+play = 1
+
+
+def buttonEventCallback(start):
+    global count
+    global time_difference
+    global play
+
+    stop = ticks_ms()
+    elapsed_time = stop - start
+
+    if elapsed_time - time_difference >= 1000 or count == 0:
+        count += 1
+        print(
+            f"Button was pressed: {count} time(s). Time since last {elapsed_time - time_difference}ms")
+        time_difference = elapsed_time
+    else:
+        print(
+            f"Ignored button press: Time left for next press is {1000 - (elapsed_time - time_difference)}ms")
+
+    mario = [E7, E7, 0, E7, 0, C7, E7, 0, G7, 0, 0, 0, G6, 0, 0, 0, C7, 0, 0, G6, 0, 0, E6, 0, 0, A6, 0, B6, 0, AS6, A6, 0, G6, E7, 0, G7, A7, 0, F7,
+             G7, 0, E7, 0, C7, D7, B6, 0, 0, C7, 0, 0, G6, 0, 0, E6, 0, 0, A6, 0, B6, 0, AS6, A6, 0, G6, E7, 0, G7, A7, 0, F7, G7, 0, E7, 0, C7, D7, B6, 0, 0]
+
+    if play:
+        play -= 1
+        mario_song(mario)
+
+
+def mario_song(mario):
+    global play
     global buzzer
+
+    led_r = Pin(15, Pin.OUT)
+    led_y = Pin(14, Pin.OUT)
+    led_g = Pin(13, Pin.OUT)
+
     for note in mario:
+        if button.value():
+            buttonEventCallback(start)
         if note < 2000:
             led_r.on()
         elif note < 3000:
             led_y.on()
         else:
             led_g.on()
-
         buzzer.play(note, 0.15)
+
         led_r.off()
         led_y.off()
         led_g.off()
-    buzzer.off()
+    play = 1
 
-
-def temperature():
-    d.measure()
-    print(f"Temprature: {round(analog_temp.temp)} °C")
-    print(f"Temprature: {d.temperature()} °C and Humidity: {d.humidity()} %")
-
-
-def main():
-    global start
-    global time_diff
-    global count
-    global play
-
-    while time_diff < 10:
-        time_diff = time() - start
-        if button.value() and time_diff >= 1:
-            count += 1
-            print(
-                f"Button was pressed: {count} time(s). Time since last " +
-                f"{time_diff}ms")
-            play = True
-            start = time()
-        elif button.value() and time_diff < 1:
-            print(
-                "Ignored button press: Time left for next press is " +
-                f"{1 - time_diff}ms")
-
-
-button = Pin(0, Pin.IN)
-
-buzzer = Speaker(1)
-
-analog_temp = TemperatureSensor(28, conversion=temp_conversion)
-
-d = dht.DHT11(Pin(27))
-
-led_r = Pin(15, Pin.OUT)
-led_y = Pin(14, Pin.OUT)
-led_g = Pin(13, Pin.OUT)
-
-play = False
-start = 0
-time_diff = 0
-count = 0
-tries = 0
 
 E7 = 2637
 F7 = 2794
@@ -92,20 +89,17 @@ AS6 = 1865
 A7 = 3520
 D7 = 2349
 
-mario = [E7, E7, 0, E7, 0, C7, E7, 0, G7, 0, 0, 0, G6, 0, 0, 0, C7, 0, 0, G6,
-         0, 0, E6, 0, 0, A6, 0, B6, 0, AS6, A6, 0, G6, E7, 0, G7, A7, 0, F7,
-         G7, 0, E7, 0, C7, D7, B6, 0, 0, C7, 0, 0, G6, 0, 0, E6, 0, 0, A6, 0,
-         B6, 0, AS6, A6, 0, G6, E7, 0, G7, A7, 0, F7, G7, 0, E7, 0, C7, D7, B6,
-         0, 0]
+buzzer = Speaker(1)
 
-_thread.start_new_thread(main, ())
+button = Pin(0, Pin.IN)
 
-start = time()
+analog_temp = TemperatureSensor(28, conversion=temp_conversion)
 
-while tries < 100:
-    sleep(0.05)
-    temperature()
-    if play:
-        mario_song()
-        sleep(0.15)
-    tries += 1
+d = dht.DHT11(Pin(27))
+
+start = ticks_ms()
+
+while True:
+    if button.value():
+        temperature()
+        buttonEventCallback(start)
