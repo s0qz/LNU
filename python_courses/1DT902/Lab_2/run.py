ae87abbc5ab7adcacea229d7ce7c4ead870c6288
Samuel Berg <sb224sc@student.lnu.se>
2022-12-05 09:28:49 +0100

    ...

--- python_courses/1DT902/Lab_2/run.py
+++ python_courses/1DT902/Lab_2/run.py
@@ -15,6 +15,8 @@ note = 2400
 
 button = Pin(0, Pin.IN)
 
+prev_msg = ''
+
 REMOTE_DIR = 'SL'
 OTHER_PI = 'l_status'
 MY_PY = 's_status'
@@ -31,9 +33,9 @@ MQTTClient.DEBUG = True
 peds_lights = 0
 
 
-def callback(topic, msg, retained):
+def callback(topic, msg, retained, qos=0):
     decoded = msg.decode('utf-8')
-    print((topic, decoded, retained))
+    print((topic, decoded, retained, qos))
 
     if decoded == 'GREEN' or decoded == 'YELLOW':
         pass
@@ -50,7 +52,6 @@ def go():
     for led in ped_leds.values():
         led.off()
     ped_leds['GREEN'].on()
-    client.publish(REMOTE_DIR + '/' + MY_PY, 'GREEN', qos=1)
     start_pedestrian_go = time()
     now = 0
     while now - start_pedestrian_go < 3:
@@ -71,9 +72,9 @@ def stop():
             buzzer.play(1000, 0.2, 0)
             now = time()
     n += 1
-    peds_lights = 0
     ped_leds['GREEN'].off()
     ped_leds['RED'].on()
+    peds_lights = 3
 
 
 def buttonEventCallback():
@@ -83,10 +84,11 @@ def buttonEventCallback():
 
 
 async def conn_han(client):
-    await client.subscribe(REMOTE_DIR + '/' + OTHER_PI, 1)
+    await client.subscribe(REMOTE_DIR + '/' + OTHER_PI)
 
 
 async def main(client):
+    global prev_msg
     global peds_lights
     await client.connect()
     for led in ped_leds.values():
@@ -97,16 +99,19 @@ async def main(client):
 
     while True:
         if peds_lights == 0:
-            lights_msg = 'RED'
+            lights_msg = 'PS'
         elif peds_lights == 1:
-            lights_msg = 'YELLOW'
-        elif peds_lights == 2:
-            lights_msg = 'GREEN'
-        print(peds_lights, lights_msg)
-        await client.publish(REMOTE_DIR + '/' + MY_PY, lights_msg, qos=1)
+            lights_msg = 'PSG'
+
+        if lights_msg != prev_msg:
+            await client.publish(REMOTE_DIR + '/' + MY_PY, lights_msg, qos=1)
+            prev_msg = lights_msg
+        if peds_lights == 3:
+            peds_lights = 0
         if button.value():
             buzzer.play(note, 0.5, 5)
             buttonEventCallback()
+        await asyncio.sleep(0.2)
 
 config['subs_cb'] = callback
 config['connect_coro'] = conn_han
