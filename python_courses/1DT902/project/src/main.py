991ddb81f8f4c8c1763e1fc424741ed24070006d
Samuel Berg <sb224sc@student.lnu.se>
2022-12-08 15:37:19 +0100

    ...

--- python_courses/1DT902/project/src/main.py
+++ python_courses/1DT902/project/src/main.py
@@ -2,9 +2,9 @@ from machine import Pin
 from time import sleep
 import dht
 
-d = dht.DHT11(Pin(0))
+dht11 = dht.DHT11(Pin(0))
 
 while True:
-    d.measure()
+    dht11.measure()
     print(f"Temprature: {d.temperature()} °C and Humidity: {d.humidity()} %")
     sleep(300)
